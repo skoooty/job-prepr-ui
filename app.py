2f886b29e6579e0004be7a390878c33c1bc8bd02
Milica Scepanovic <milicascepanovic997@gmail.com>
2022-09-06 15:44:18 +0100

    2022-09-06 15:43 Website version 1: Video recording + Frames extraction + Questions pop up

--- app.py
+++ app.py
@@ -1,34 +1,106 @@
-import threading
-
-import cv2
 import streamlit as st
-from matplotlib import pyplot as plt
+from streamlit_webrtc import (
+    RTCConfiguration,
+    VideoProcessorBase,
+    webrtc_streamer,
+)
 
-from streamlit_webrtc import webrtc_streamer
+from PIL import Image
+import numpy as np
+import cv2
+import threading
+import pandas as pd
+from random import randint
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 lock = threading.Lock()
-img_container = {"img": None}
+img_container = {"frames":[]}
 
+class VideoProcessor(VideoProcessorBase):
+    def __init__(self):
+        self.style = 'color'
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
+    def video_frame_callback(self):
+        img = self.to_ndarray(format="bgr24")
+        img = find_face(img)
+        with lock:
+            if img is not None:
+                img_container["frames"].append(img)
 
-    return frame
+def find_face(image):
+    try:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
-ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+        faces = face_cascade.detectMultiScale(
+                                                image,
+                                                scaleFactor=1.3,
+                                                minNeighbors=3,
+                                                minSize=(30, 30)
+                                                        )
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            just_face = image[y:y + h, x:x + w]
+            just_face=np.array(Image.fromarray(just_face).resize((48,48)))
+            return just_face
+    except:
+        return None
 
-fig_place = st.empty()
-fig, ax = plt.subplots(1, 1)
+def load_questions():
+    questions = pd.read_csv('interview_questions.csv',
+                            header=0,
+                            names=["Area", "Question"],
+                            on_bad_lines='skip',
+                            delimiter=";")
+    return questions
 
-while ctx.state.playing:
-    with lock:
-        img = img_container["img"]
-    if img is None:
-        continue
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ax.cla()
-    ax.hist(gray.ravel(), 256, [0, 256])
-    fig_place.pyplot(fig)
+def get_rand_question(questions, job_name):
+    questions=questions[questions["Area"]==job_name]
+    no_of_q=questions.shape[0]
+    index=randint(0, no_of_q-1)
+    return questions["Question"].iloc[index]
+
+def main():
+    questions=load_questions()
+    job_list = questions["Area"].unique()
+    with st.sidebar:
+        st.title('Area Selection')
+        job_name = st.selectbox("Select the job that you are applying for:", job_list)
+    st.title("How confident are you?")
+    st.write(f"Let's practice for your {job_name} interview.")
+    playing = st.checkbox("Start the session.", value=False)
+
+    if playing:
+        question=get_rand_question(questions, job_name)
+        st.write(question)
+        webrtc_streamer(
+            key="object-detection",
+            video_frame_callback=VideoProcessor.video_frame_callback,
+            media_stream_constraints={
+                "video": True,
+                "audio": False
+            },
+            desired_playing_state=playing
+        )
+
+    if 'frames' not in st.session_state:
+        st.session_state['frames'] = img_container["frames"]
+    if 'all_faces' not in st.session_state:
+        st.session_state['all_faces'] = []
+
+    else:
+        if len(st.session_state['frames']) < 1:
+            st.session_state['frames'] = img_container["frames"]
+
+    if not playing:
+        frames=st.session_state["frames"]
+        frames=frames[::15]
+        if len(frames):
+            st.write(f"We've collected {len(frames)} frames...")
+        for frame in frames:
+            st.image(frame)
+
+if __name__ == "__main__":
+    main()
