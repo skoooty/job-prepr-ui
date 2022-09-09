3019fb4eac976f12a9264dd972d4923ac804a45f
Milica Scepanovic <milicascepanovic997@gmail.com>
2022-09-09 17:02:02 +0100

    Setup page

---
+++ pages/02_Tutorial.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import imageio
+
+def main():
+    st.title("How to use our app?")
+    st.write("JobPrepr aims to help professionals prepare better for their video interviews.")
+
+
+if __name__ == "__main__":
+    main()
