15374803b563d981e8cbfc95ce6afa536c7fba79
Huw Barber <huwbarber@gmail.com>
2022-09-09 17:00:42 +0100

    Added return in db_queries.py

--- utils/db_queries.py
+++ utils/db_queries.py
@@ -9,4 +9,6 @@ def create_new_user(email: str, password: str) -> None:
     salt, hashed_password = hash_password(password)
 
     with pool.connect() as db_conn:
-        db_conn.execute("INSERT INTO users (email, password, salt) VALUES (?,?,?)", (email, hashed_password, salt))
+        db_conn.execute("INSERT INTO users (email, password, salt) VALUES (?,?,?)", (email, hashed_password, salt,))
+
+    return None
