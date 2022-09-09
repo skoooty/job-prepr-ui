4dc81de6386bbcd8d0bff1e743eb929c470586da
Huw Barber <huwbarber@gmail.com>
2022-09-09 16:33:55 +0100

    Created connection to GCloud DB + Began query construction

---
+++ utils/db_queries.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from db_connect import create_pool
+from db_password import hash_password
+
+def create_new_user(email: str, password: str) -> None:
+
+    pool = create_pool()
+
+    salt, hashed_password = hash_password(password)
+
+    with pool.connect() as db_conn:
+        db_conn.execute("INSERT INTO users (email, password, salt) VALUES (?,?,?)", (email, hashed_password, salt))
